84f200639b1a003312f57e92cb5c05753b55d381
Greg Little <glittle@gmail.com>
2014-01-19 00:14:38 -0700

    init

---
+++ utils.js
@@ -0,0 +1,110 @@
+
+function grid(rows) {
+    var t = []
+    t.push('<table style="width:100%;height:100%">')
+    _.each(rows, function (row, y) {
+        t.push('<tr height="33.33%">')
+        _.each(row, function (cell, x) {
+            var c = 'x' + x + 'y' + y
+            t.push('<td class="' + c + '" width="33.33%"/>')
+        })
+        t.push('</tr>')
+    })
+    t.push('</table>')
+    t = $(t.join(''))
+
+    _.each(rows, function (row, y) {
+        _.each(row, function (cell, x) {
+            var c = 'x' + x + 'y' + y
+            t.find('.' + c).append(cell)
+        })
+    })
+
+    return t
+}
+
+function center(me) {
+    var t = $('<table style="width:100%;height:100%"><tr><td valign="center" align="center"></td></tr></table>')
+    t.find('td').append(me)
+    return t
+}
+
+$.fn.myAppend = function (args) {
+    for (var i = 0; i < arguments.length; i++) {
+        var a = arguments[i]
+        if (a instanceof Array)
+            $.fn.myAppend.apply(this, a)
+        else
+            this.append(a)
+    }
+    return this
+}
+
+function cssMap(s) {
+    var m = {}
+    _.each(s.split(';'), function (s) {
+        var a = s.split(':')
+        if (a[0])
+            m[_.trim(a[0])] = _.trim(a[1])
+    })
+    return m
+}
+
+$.fn.myCss = function (s) {
+    return this.css(cssMap(s))
+}
+
+$.fn.myHover = function (s, that) {
+    var that = that || this
+    var m = cssMap(s)
+    var old = _.map(m, function (v, k) {
+        return that.css(k)
+    })
+    this.hover(function () {
+        that.css(m)
+    }, function () {
+        that.css(old)
+    })
+    return this
+}
+
+$.fn.addLabel = function (d) {
+    if (typeof(d) == "string") d = $('<span/>').text(d)
+        
+    var id = _.randomString(10, /[a-z]/)
+    this.attr('id', id)
+    this.after($('<label for="' + id + '"/>').append(d))
+    return this
+}
+
+
+function rotate(me, amount) {
+    var s = 'rotate(' + amount + 'deg)'
+    me.css({
+        '-ms-transform' : s,
+        '-moz-transform' : s,
+        '-webkit-transform' : s,
+        '-o-transform' : s
+    })
+    return me
+}
+
+jQuery.fn.extend({
+    rotate : function (amount) {
+        return this.each(function () {
+            rotate($(this), amount)
+        })
+    }
+})
+
+function createThrobber() {
+    var d = $('<div/>').text('.')
+    var start = _.time()
+    var i = setInterval(function () {
+        if ($.contains(document.documentElement, d[0])) {
+            d.rotate(Math.round((_.time() - start) / 1000 * 360 * 2 % 360))
+        } else
+            clearInterval(i)
+    }, 30)
+    return d;
+}
