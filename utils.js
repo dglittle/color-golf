d754ed9be5966cf1b49e544e4047041c5e07fd19
Greg Little <glittle@gmail.com>
2014-01-22 05:52:03 -0700

    blah

--- utils.js
+++ utils.js
@@ -1,4 +1,47 @@
 
+function drawShareButtons(message, url, cb) {
+    var d = $('<div/>')
+
+    var shares = [
+        {
+            type : 'facebook',
+            img : 'facebook_grey.png',
+            url : createFacebookShareLink(url, '', message, '')
+        },
+        {
+            type : 'twitter',
+            img : 'twitter_grey.png',
+            url : createTwitterShareLink(message + ' ' + url)
+        },
+        {
+            type : 'google+',
+            img : 'google_plus_grey.png',
+            url : createGooglePlusShareLink(url)
+        }
+    ]
+
+    _.each(shares, function (share, i) {
+        d.append($('<img style="cursor:pointer;margin-right:10px"/>').attr('src', share.img).click(function () {
+            cb(share.type)
+            window.open(share.url, 'share url', 'height=400,width=500,resizable=yes')
+        }))
+    })
+
+    return d
+}
+
+function createFacebookShareLink(url, img, title, summary) {
+    return 'http://www.facebook.com/sharer/sharer.php?s=100&p[url]=' + _.escapeUrl(url) + '&p[images][0]=' + _.escapeUrl(img) + '&p[title]=' + _.escapeUrl(title) + '&p[summary]=' + _.escapeUrl(summary)
+}
+
+function createTwitterShareLink(tweet) {
+    return 'http://twitter.com/home?status=' + _.escapeUrl(tweet)
+}
+
+function createGooglePlusShareLink(url) {
+    return 'https://plus.google.com/share?url=' + _.escapeUrl(url)
+}
+
 function grid(rows) {
     var t = []
     t.push('<table style="width:100%;height:100%">')
